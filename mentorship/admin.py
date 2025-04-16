c74c799eebf669f14e3e82816d50a065f71e7804
showq0 <showqbalawneh@gmail.com>
2025-04-16 11:42:52 +0300

    updateUserform to fix hashing pasword

--- mentorship/admin.py
+++ mentorship/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from mentorship.models import User, Mentorship, BookingSlot, Session
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None
+         ,
+         {"fields": ("role", "profile_info"),}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None
+         , {"fields": ("role", "profile_info")}),
+    )
+
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         form.base_fields['profile_info'].required = False
