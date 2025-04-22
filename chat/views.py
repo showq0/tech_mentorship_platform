63b0d4587bc595d1a84d6093089bbab1a6be4fa9
showq0 <showqbalawneh@gmail.com>
2025-04-22 14:38:28 +0300

    endpoint & pagination

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from mentorship.serializers import UserSerializer
+from chat.models import Message, Chat, User
+from chat.pagination import ViewPagination
 
-# Create your views here.
+
+class ConversationsListView(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = ViewPagination
+
+    def get_queryset(self):
+        if self.request.user:
+            return Chat.get_user_chats(self.request.user)
+        return []
+
