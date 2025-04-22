c9ac756ec6d85950715b623db16a268378480300
showq0 <showqbalawneh@gmail.com>
2025-04-22 14:14:04 +0300

    rename serializer

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from user_auth.serializer import RegisterSerializer
+from user_auth.serializers import RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
