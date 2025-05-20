24ec32a2aea2b9faa3d409271a575a58320c0b44
showq0 <showqbalawneh@gmail.com>
2025-05-20 17:14:46 +0300

    revert asgi edit

--- tech_mentorship_platform/asgi.py
+++ tech_mentorship_platform/asgi.py
@@ -8,10 +8,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_mentorship_platform.settings')
-django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
