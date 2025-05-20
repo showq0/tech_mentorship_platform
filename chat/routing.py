c60de350d109994a53bbc8ba5a7508c89bfc8d75
showq0 <showqbalawneh@gmail.com>
2025-05-20 18:27:43 +0300

    routing ws

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from chat.consumers import MessagesConsumer
 
 websocket_urlpatterns = [
-    re_path(r'wss/messages/(?P<chat_id>\d+)/$', MessagesConsumer.as_asgi()),
+    re_path(r'ws/messages/(?P<chat_id>\d+)/$', MessagesConsumer.as_asgi()),
 ]
