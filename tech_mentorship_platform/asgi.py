2e52255bc2be58ec4390c364a293f6d5e8bd0cb7
showq0 <showqbalawneh@gmail.com>
2025-05-20 18:55:39 +0300

    dj setup in asgi

--- tech_mentorship_platform/asgi.py
+++ tech_mentorship_platform/asgi.py
@@ -13,11 +13,12 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_mentorship_platform.settings')
 
 django.setup()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
